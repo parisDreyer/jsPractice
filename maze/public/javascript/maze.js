91224e4ebda05f27d77aca03a1b80db3e01e7fe7
Luke Dreyer <lukepdreyer@gmail.com>
2019-01-20 11:27:56 -0800

    make maze alot better though still doesnt work 100 percent of the time

--- maze/public/javascript/maze.js
+++ maze/public/javascript/maze.js
@@ -1,5 +1,3 @@
-
-
 // builds NxN maze with walls between random points
 function buildMaze(n, wall = "u", space = " ", start = "%s", end ="%e"){
     let grid = [];
@@ -17,7 +15,7 @@ function buildMaze(n, wall = "u", space = " ", start = "%s", end ="%e"){
     }
     let { start_coord, end_coord } = setStartAndEndCoordinates(grid, n, start, end);
     let paths = drawPathToEnd(grid, [start_coord.y, start_coord.x], [end_coord.y, end_coord.x], start, space, end);
-    plotPointsOnGrid(grid, paths, space);
+    Object.keys(paths).map(y => paths[y].forEach(x => (x ? (grid[y][x] = wall) : false)));
     
     for(let i = 0; i < n; ++i){
         fillInWalls(grid, paths, n, i, wall, space);
@@ -26,55 +24,67 @@ function buildMaze(n, wall = "u", space = " ", start = "%s", end ="%e"){
     return grid;
 }
 
-function plotPointsOnGrid(grid, points, mark){
-    for(let i = 0; i < points.length; ++i){
-        let [y, x] = points[i];
-        grid[y][x] = mark;
-    }
-}
+    
 
-// draws a long winding path to end
+
+// helper for buildMaze(). Draws a long winding path to end_pos
 // returns a hash of coordinates that represent the path
 function drawPathToEnd(grid, start_pos, end_pos, start, space, end){
+    // some variables for the calculation
     let n = grid.length;
     let max = n - 2, min = 1;
-    let path = {};              // the return value of this function
+    let max_path_length = Math.floor((n / 2) * n);
+    let curr_path_length = 0;
+    let path = {};  // path contains arrays of booleans 
+                    // specifying whether a given index
+                    // is an x coordinate in the path
+                    // the return value of this function
     let [y, x] = start_pos;
     let still_searching = true; // y != end_pos[0] && x != end_pos[1];
-    let counter = 0;            // used for limiting movement in a given direction
+    let counter = 0; // used for limiting movement in a given direction
+
+    // helper functions for adding and removing coordinates from grid
 
     const checkEndReached = () => {
         if(y > 0 && y < n && grid[y][x] === end) {
-            ////console.log('end reached', [y, x]);
             still_searching = false;
-            // path.push([y, x]);
             return true;
         } else return false;
     }
-
+    const add_coord = () => { 
+        if (!path[y] && grid[y]) path[y] = [];
+        if(path[y]){
+            for(let i = path[y].length; i <= x; ++i){ path[y].push(false); }
+            path[y][x] = true; // x is in the path;
+        } return path[y] && path[y][x];
+    }
     const updatePath = () => {
-        let signal = false; // signal whether to return path
         if (y > 0 && y < n && grid[y][x] != start) {
-          grid[y][x] = space; // set the next step in the path
-          if (!path[y]) {
-              path[y] = [x];
-              signal = true;
-          }else if (!path[y].includes(x)) {
-            path[y].push(x);
-              signal = true;
-          }
-        } return signal;
+          curr_path_length++;
+          // grid[y][x] = space; // set the next step in the path
+          return add_coord();
+        } return false;
     };
 
-    while(still_searching){ 
+    const isGoodDistanceFromEnd = () =>  // check if we've done enough meandering
+        (max_path_length - curr_path_length) >
+        (Math.abs(y - end_pos[0]) + Math.abs(x - end_pos[1])) + 1;
+    
+    // ========================================
+    // until we have a good bunch of meandering, keep adding a meandering path
+    let prevX = null, prevY = null;
+    while(still_searching && isGoodDistanceFromEnd()){
+        let good_horizontal_dist = Math.abs(y - prevY) > 2;
         let left = Math.random() > 0.495;
-        let up = Math.random() < 0.495;
+        let up = (!good_horizontal_dist || Math.abs(x - prevX) > 2) && Math.random() < 0.495; 
         if(end_pos[0] > y) diry = 1;
         else if (end_pos[0] < y) diry = -1; 
         if (end_pos[1] > x) dirx = 1;
         else if (end_pos[1] < x) dirx = -1;
-
+        prevX = x, prevY = y;
         counter = randInRange(2, n / 2);
+
+        if(good_horizontal_dist){
         if(left){
             while (counter > 0 && (x >= min || (y === max))){
                 if (checkEndReached()) return path
@@ -90,6 +100,7 @@ function drawPathToEnd(grid, start_pos, end_pos, start, space, end){
                 counter--;
             }
         }
+    }
         counter = randInRange(2, n / 2);
         if(up){
             while (counter > 0 && (y >= min || x === start_pos[0])) {
@@ -106,30 +117,72 @@ function drawPathToEnd(grid, start_pos, end_pos, start, space, end){
                 counter--;
             }
         }
-        //console.log(`y: ${y}  x: ${x}`, end_pos);
         still_searching = y != end_pos[0] || x != end_pos[1];
         if (x < min) { x = min; }
         if (x > max) { x = max; }
         if (y < min) { y = min; }
         if (y > max) { y = max; }
-        //console.log('still_searching: ', still_searching);
+    }
+
+
+    // ========================================
+    // if not at end after meandering get to end, 
+    // also remove any duplicate ways to get to the end
+    // ==============================
+
+    let startX = x, startY = y;
+    let startXjunction = x, startYjunction = y;
+    // removes a coordinate from the path
+    const subtract_coord = (ny, nx) => {
+        if (path[ny] && path[ny][nx] 
+            && grid[ny][nx] != end 
+            && grid[ny][nx] != start
+            && ny != startY
+            && nx != startX){
+            path[ny][nx] = false; // nx is no longer the path;
+        }
+    }
+    
+    let dx = x - end_pos[1], dy = y - end_pos[0];
+    dx = dx / Math.abs(dx), dy = dy / Math.abs(dy); // normalize
+    const bringXHome = () => {  // get x to end pos x
+        while(x != end_pos[1]){
+            x -= dx;
+            add_coord();
+            subtract_coord(y - 1, x);  // remove extra possible paths from start to end
+            subtract_coord(y + 1, x); 
+        }
+    }
+    const bringYHome = () => {  // gets y to end pos y
+        while (y != end_pos[0]) {
+            y -= dy;
+            add_coord();
+            subtract_coord(y, x - 1); // remove extra possible paths from start to end
+            subtract_coord(y, x + 1);
+        }
+    }
+    let doXfirst = Math.round(Math.random()) > 0; // randomly choose to bring x or y home first
+    if(doXfirst){
+        bringXHome();
+        bringYHome();
+    } else {
+        bringYHome();
+        bringXHome();
     }
     return path;
-}
+}   // end function drawPathToEnd
 
 function setRand2block(grid, y, x, diry, dirx, block, player_path){
     let vert = Math.round(Math.random()) > 0; // if true vertical, else horizontal
-    // console.log(y, x);
-    // console.log(player_path);
-    let hasx = player_path[y] ? player_path[y].includes(x) : false;
+    let hasx = player_path[y] ? player_path[y][x] : false;
     if (vert) {
         if(!hasx) grid[y][x] = block;
         let mody = y + diry;
-        if (!player_path[mody] || !player_path[mody].includes(x)) grid[mody][x] = block;
+        if (!player_path[mody] || !player_path[mody][x]) grid[mody][x] = block;
     } else {
         if (!hasx) grid[y][x] = block;
         let modx = x + dirx;
-        if (!player_path[y] || !player_path[y].includes(modx)) grid[y][modx] = block;
+        if (!player_path[y] || !player_path[y][modx]) grid[y][modx] = block;
     }
 }
 // helper methods:
